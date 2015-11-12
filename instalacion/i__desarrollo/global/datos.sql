ab5bd9d4cc8200727c9c7b1e6488ffd621b90520
fernandojavierguerra <fernandojavierguerra@gmail.com>
2015-11-12 12:55:33 -0300

    Commit incial

---
+++ instalacion/i__desarrollo/global/datos.sql
@@ -0,0 +1,2 @@
+INSERT INTO apex_revision (revision, creacion) VALUES ('DESCONOCIDA', '2015-11-04 12:54:30');
+INSERT INTO apex_instancia (instancia, version, institucion, observaciones, administrador_1, administrador_2, administrador_3, creacion) VALUES ('desarrollo', '2.7.2', NULL, NULL, NULL, NULL, NULL, '2015-11-04 12:54:30');
---
+++ instalacion/i__desarrollo/p__comunidades/datos.sql
@@ -0,0 +1,1 @@
+INSERT INTO apex_admin_param_previsualizazion (proyecto, usuario, grupo_acceso, punto_acceso, perfil_datos) VALUES ('comunidades', 'toba', 'admin', '/comunidades/1.0', NULL);
