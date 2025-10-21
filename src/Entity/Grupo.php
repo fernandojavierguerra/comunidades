a1eb28b488dbf77da71e5bab3a784422b2b5849a
fernandojavierguerra@gmail.com <fernandojavierguerra@gmail.com>
2025-10-21 10:11:36 -0300

    Grupo

---
+++ src/Entity/Grupo.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GrupoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GrupoRepository::class)]
+class Grupo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 80)]
+    private ?string $grupo = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Comunidad $comunidad = null;
+
+    #[ORM\Column]
+    private ?int $periodo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGrupo(): ?string
+    {
+        return $this->grupo;
+    }
+
+    public function setGrupo(string $grupo): static
+    {
+        $this->grupo = $grupo;
+
+        return $this;
+    }
+
+    public function getComunidad(): ?Comunidad
+    {
+        return $this->comunidad;
+    }
+
+    public function setComunidad(?Comunidad $comunidad): static
+    {
+        $this->comunidad = $comunidad;
+
+        return $this;
+    }
+
+    public function getPeriodo(): ?int
+    {
+        return $this->periodo;
+    }
+
+    public function setPeriodo(int $periodo): static
+    {
+        $this->periodo = $periodo;
+
+        return $this;
+    }
+}
