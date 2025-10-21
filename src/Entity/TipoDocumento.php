7f979ece813a6e59aa7ebf9333a148975a87be10
fernandojavierguerra@gmail.com <fernandojavierguerra@gmail.com>
2025-10-21 10:03:04 -0300

    Tipo documento

---
+++ src/Entity/TipoDocumento.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TipoDocumentoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TipoDocumentoRepository::class)]
+class TipoDocumento
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $tipoDocumento = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Comunidad $comunidad = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTipoDocumento(): ?string
+    {
+        return $this->tipoDocumento;
+    }
+
+    public function setTipoDocumento(string $tipoDocumento): static
+    {
+        $this->tipoDocumento = $tipoDocumento;
+
+        return $this;
+    }
+
+    public function getComunidad(): ?Comunidad
+    {
+        return $this->comunidad;
+    }
+
+    public function setComunidad(?Comunidad $comunidad): static
+    {
+        $this->comunidad = $comunidad;
+
+        return $this;
+    }
+}
---
+++ src/Repository/TipoDocumentoRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TipoDocumento;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<TipoDocumento>
+ */
+class TipoDocumentoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TipoDocumento::class);
+    }
+
+    //    /**
+    //     * @return TipoDocumento[] Returns an array of TipoDocumento objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?TipoDocumento
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
